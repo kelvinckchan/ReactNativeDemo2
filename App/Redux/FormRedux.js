0f728648e2ac5cdceb303a86814a10141b521e66
kelvin <kelvinckchan94@gmail.com>
2017-08-31 17:27:23 +0800

    update

---
+++ App/Redux/FormRedux.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
+const reducers = {
+  // ... your other reducers here ...
+  form: formReducer     // <---- Mounted at 'form'
+}
+const reducer = combineReducers(reducers)
+const store = createStore(reducer)
